2da9acad1dfcf9c9df048b606a79ef51421a53c3
IcaroTeles <114035956+IcaroTeles@users.noreply.github.com>
2025-05-26 21:11:37 +0000

    IMPLEMENTAÇÃO DE DENSIDADE POPULACIONAL E PER CAPTA

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ int main() {
 
     char estado1, estado2, cidade1[20], cidade2[20], codigo1[2], codigo2[2];
     int  pontos1, pontos2, populacao1, populacao2;
-    float area1, area2, pib1, pib2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, percapta1, percapta2;
 
     // CADASTRO CARTA 1: diálogos de interação e scan de respostas:
 
@@ -39,14 +39,21 @@ int main() {
     printf("E, por último, quantos pontos turísticos ela tem?\n");
     scanf("%d", &pontos1);
 
+    //CALCULO DE DENSIDADE E PER CAPTA
+
+    densidade1 = (float)area1 / populacao1;
+    percapta1= (float) pib1 / populacao1;
+
     // Exibição dos Dados das Cartas:
 
     printf("\nAcabamos o nosso cadastro! Agora vou colocar todas as infos aqui embaixo:\n");
     printf("Sua carta cadastrada é a de código: %c%s\n", estado1, codigo1);
     printf("Nome da cidade: %s\n", cidade1);
     printf("População: %d habitantes\n", populacao1);
-    printf("Área: %.f m²\n", area1);  
-    printf("PIB: R$%.f \n", pib1); 
+    printf("Área: %.2f m²\n", area1);
+    printf("Densidade Demográfica de: %.2f pessoas/m²\n", densidade1);   
+    printf("PIB: R$%.2f \n", pib1);
+    printf("PIB per Capta: R$%.2f \n", percapta1); 
     printf("Pontos Turísticos: %d\n", pontos1);
     
     //Saudação Final de confirmação:
@@ -78,14 +85,21 @@ int main() {
     printf("E, por último, quantos pontos turísticos ela tem?\n");
     scanf("%d", &pontos2);
 
+    //CALCULO DE DENSIDADE E PER CAPTA
+
+    densidade2 = (float) area2 / populacao2;
+    percapta2= (float) pib2 / populacao2;
+
     // Exibição dos Dados das Cartas:
 
     printf("\nAcabamos o nosso cadastro! Agora vou colocar todas as infos aqui embaixo:\n");
     printf("Sua carta cadastrada é a de código: %c%s\n", estado2, codigo2);
     printf("Nome da cidade: %s\n", cidade2);
     printf("População: %d habitantes\n", populacao2);
-    printf("Área: %.f m²\n", area2);  
-    printf("PIB: R$%.f \n", pib2); 
+    printf("Área: %.2f m²\n", area2);
+    printf("Densidade Demográfica de: %.2f pessoas/m²\n", densidade2);  
+    printf("PIB: R$%.2f \n", pib2);
+    printf("PIB per Capta: R$%.2f \n", percapta2);  
     printf("Pontos Turísticos: %d\n", pontos2);
     
     //Saudação Final de confirmação:
